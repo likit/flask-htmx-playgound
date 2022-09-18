befab6f3dcc0bb82f4b758d2987c81b8b09a1240
Likit Preeyanon <likit.pre@mahidol.edu>
2022-09-19 01:16:54 +0700

    added modal form demo with realtime update of a new entry

--- app/form/views.py
+++ app/form/views.py
@@ -1,8 +1,10 @@
-from flask import render_template, request
+from flask import render_template, request, make_response
 
 from app.form import form_bp
 from app.form.forms import UserForm
 
+
+users = []
 
 @form_bp.route('/field-list')
 def demo_field_list():
@@ -36,3 +38,25 @@ def demo_form_modal():
 @form_bp.route('/modal1_template')
 def get_modal1_template():
     return render_template('form/modal1.html')
+
+
+@form_bp.route('/modal2_template')
+def get_modal2_template():
+    form = UserForm()
+    return render_template('form/modal2.html', form=form)
+
+
+@form_bp.route('/modal2/process', methods=['GET', 'POST'])
+def modal2_form():
+    form = UserForm()
+    if request.method == 'GET':
+        return render_template('form/form_modal2.html')
+    if form.validate_on_submit():
+        resp = make_response('''
+        <tr>
+            <td>{}</td><td>{}</td>
+        </tr>
+        '''.format(form.name.data, form.emails[0].email.data)
+        )
+        resp.headers['HX-Trigger-After-Swap'] = 'closeModal'
+        return resp
